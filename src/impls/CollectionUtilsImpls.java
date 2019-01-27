e9625f432001d9716e66003f355cf135ed97cb60
alexandrFrolov2093 <sany122334@gmail.com>
2019-01-27 13:37:00 +0300

    CollectionUtilsImpls

--- src/impls/CollectionUtilsImpls.java
+++ src/impls/CollectionUtilsImpls.java
@@ -4,27 +4,64 @@ import interfaces2.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public class CollectionUtilsImpls implements CollectionUtils {
     @Override
     public Collection<Integer> union(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        if(a == null || b == null)
+            throw new NullPointerException("a == null || b == null");
+        ArrayList<Integer> result = new ArrayList();
+        result.addAll(a);
+        result.addAll(b);
+        return result;
     }
 
     @Override
     public Collection<Integer> intersection(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        if(a == null || b == null){
+            throw new NullPointerException("a == null || b == null");
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for(Integer num: a){
+            if(b.contains(num))
+                result.add(num);
+        }
+        for(Integer num: b){
+            if(a.contains(num))
+                result.add(num);
+        }
+        return result;
     }
 
     @Override
     public Set<Integer> unionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        if(a == null || b == null){
+            throw new NullPointerException("a == null || b == null");
+        }
+        HashSet<Integer> result = new HashSet<>();
+        result.addAll(a);
+        result.addAll(b);
+
+        return result;
     }
 
     @Override
     public Set<Integer> intersectionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        if(a == null || b == null){
+            throw new NullPointerException("a == null || b == null");
+        }
+        HashSet<Integer> result = new HashSet<>();
+        for(Integer num: a){
+            if(b.contains(num))
+                result.add(num);
+        }
+        for(Integer num: b){
+            if(a.contains(num))
+                result.add(num);
+        }
+        return result;
     }
 
     @Override
